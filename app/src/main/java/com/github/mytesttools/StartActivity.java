b87260fc0e382da3e387550570575cbaf11a0fbd
Torick <weiyq12@qq.com>
2016-07-24 00:17:31 +0800

    update StartActivity

--- app/src/main/java/com/github/mytesttools/StartActivity.java
+++ app/src/main/java/com/github/mytesttools/StartActivity.java
@@ -3,10 +3,11 @@ package com.github.mytesttools;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StartActivity extends AppCompatActivity {
-
+public class StartActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
